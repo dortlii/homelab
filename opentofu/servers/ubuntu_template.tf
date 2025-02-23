3ee7a3e2163a506cffa9f03bbf284046a7aca025
dortlii <22558338+dortlii@users.noreply.github.com>
2025-02-23 03:06:18 +0100

    Add ubuntu server template

---
+++ opentofu/servers/ubuntu_template.tf
@@ -0,0 +1,64 @@
+locals {
+  ubuntu_img_url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+}
+
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = local.iso_datastore
+  node_name    = local.node
+
+  url = local.ubuntu_img_url
+}
+
+resource "proxmox_virtual_environment_vm" "ubuntu_template" {
+  name      = "ubuntu-template"
+  node_name = local.node
+
+  template = true
+
+  machine     = "q35"
+  bios        = "ovmf"
+  description = "Managed by Terraform"
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  efi_disk {
+    datastore_id = "local"
+    file_format  = "raw"
+    type         = "4m"
+  }
+
+  disk {
+    datastore_id = "local"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
+  }
+
+  network_device {
+    bridge = "vmbr1"
+  }
+
+}
