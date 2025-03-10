0e69a81ae856bca4909cbb7e2c47d123c3b0dc0a
dortlii <22558338+dortlii@users.noreply.github.com>
2025-03-10 21:16:40 +0100

    Add Proxmox provider configuration and update module references

--- opentofu/environments/dev/main.tf
+++ opentofu/environments/dev/main.tf
@@ -1,21 +1,33 @@
 terraform {
-  backend "local" {}  # Or use S3, GitLab, etc.
+  backend "local" {} # Or use S3, GitLab, etc.
 }
 
-module "global" {
-  source = "../../global"
+provider "proxmox" {
+  endpoint  = var.proxmox_api_url
+  api_token = var.proxmox_api_token
+  insecure  = true
+  ssh {
+    agent    = true
+    username = var.proxmox_username
+    password = var.proxmox_password
+  }
+}
 
-  proxmox_api_url   = var.proxmox_api_url
-  proxmox_api_token = var.proxmox_api_token
-  proxmox_username  = var.proxmox_username
-  proxmox_password  = var.proxmox_password
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.73.0"
+    }
+  }
 }
 
 module "vm" {
   source = "../../modules/vm"
-  providers = {
-    proxmox = module.global.proxmox
-  }
+
+  proxmox_api_url = var.proxmox_api_url
+  proxmox_password = var.proxmox_password
+  proxmox_api_token = var.proxmox_api_token
 
   ssh_public_key_path = "../../../homelab-files/opentofu/ssh.pub"
 
@@ -35,5 +47,5 @@ module "vm" {
   address        = each.value.ip_address
   gateway        = each.value.gateway
   nameserver     = each.value.nameserver
-  network_bridge = each.value.network_bridge
+  network_bridge  = each.value.network_bridge
 }
--- opentofu/global/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "proxmox" {
-  value = proxmox.global
-}
