6068a2a38f8e4d47b796044f5f7b0bf83513d3b4
dortlii <22558338+dortlii@users.noreply.github.com>
2025-04-10 20:41:17 +0200

    Add Terraform configuration for Proxmox virtual machines with variables and outputs

---
+++ opentofu/modules/vm-iso/main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.74.1"
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "vm" {
+
+  lifecycle {
+    ignore_changes = [
+      started,
+      initialization
+    ]
+  }
+
+  name      = var.name
+  node_name = var.target_node
+  vm_id     = var.id
+
+  template = var.template
+
+  machine     = var.machine
+  bios        = var.bios
+  description = var.description
+
+  agent {
+    enabled = var.agent_enabled
+  }
+
+  cpu {
+    cores   = var.cpu_cores
+    sockets = var.cpu_sockets
+    type    = var.cpu_type
+  }
+
+  memory {
+    dedicated = var.memory
+  }
+
+  efi_disk {
+    datastore_id = "local-lvm"
+    file_format  = "raw"
+    type         = "4m"
+  }
+
+  cdrom {
+    file_id      = var.iso
+    interface    = "ide2"
+  }
+
+  boot_order = ["ide2", "virtio0"]
+
+  disk {
+    interface    = "virtio0"
+    size         = var.os_disk_size
+    datastore_id = var.datastore
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = var.address
+        gateway = var.gateway
+      }
+    }
+
+    dns {
+      servers = [var.nameserver]
+    }
+
+    user_data_file_id = var.user_data_cloud_config_id
+  }
+
+  network_device {
+    bridge = var.network_bridge
+    vlan_id = var.vlan_id
+  }
+
+}
---
+++ opentofu/modules/vm-iso/variables.tf
@@ -0,0 +1,117 @@
+# VM module variables
+variable "name" {
+  description = "The name of the VM"
+  type        = string
+}
+
+variable "target_node" {
+  description = "The Proxmox node to deploy to"
+  type        = string
+}
+
+variable "id" {
+  description = "The id of the VM"
+  type        = number
+}
+
+variable "template" {
+  description = "The name of the template to clone"
+  type        = bool
+  default     = false
+}
+
+variable "machine" {
+  description = "The machine type"
+  type        = string
+  default     = "q35"
+}
+
+variable "bios" {
+  description = "The BIOS type"
+  type        = string
+  default     = "ovmf"
+}
+
+variable "description" {
+  description = "Description of the VM"
+  type        = string
+}
+
+variable "agent_enabled" {
+  description = "Enable the agent"
+  type        = bool
+  default     = true
+}
+
+variable "cpu_cores" {
+  description = "Number of CPU cores"
+  type        = number
+  default     = 1
+}
+
+variable "cpu_sockets" {
+  description = "Number of CPU sockets"
+  type        = number
+  default     = 1
+}
+
+variable "cpu_type" {
+  description = "CPU type"
+  type        = string
+  default     = "host"
+}
+
+variable "memory" {
+  description = "Memory in MB"
+  type        = number
+  default     = 2048
+}
+
+variable "os_disk_size" {
+  description = "Size of the disk"
+  type        = string
+  default     = "20G"
+}
+
+variable "datastore" {
+  description = "Datastore to use"
+  type        = string
+  default     = "local-lvm"
+}
+
+variable "network_bridge" {
+  description = "The network bridge to use"
+  type        = string
+  default     = "vmbr1"
+}
+
+variable "address" {
+  description = "The IP address of the VM"
+  type        = string
+}
+
+variable "gateway" {
+  description = "The gateway of the VM"
+  type        = string
+}
+
+variable "nameserver" {
+  description = "The nameserver of the VM"
+  type        = string
+}
+
+variable "iso" {
+    description = "The ID of the ISO file to use"
+    type        = string
+}
+
+variable "user_data_cloud_config_id" {
+    description = "The ID of the cloud-init user data file"
+    type        = string
+}
+
+variable "vlan_id" {
+    description = "The VLAN ID to use"
+    type        = number
+    default     = 10
+}
