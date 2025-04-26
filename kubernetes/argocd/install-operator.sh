a154440ac7647bf2723ab8e8e96fb21ad87954ec
dortlii <22558338+dortlii@users.noreply.github.com>
2025-04-26 23:41:44 +0200

    Add install script for ArgoCD operator and CRDs

--- kubernetes/argocd/install-operator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# install operator and crds
-kubectl create -f https://operatorhub.io/install/argocd-operator.yaml
---
+++ kubernetes/argocd/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install operator and crds
+# kubectl create -f https://operatorhub.io/install/argocd-operator.yaml
+
+# https://kostis-argo-cd.readthedocs.io/en/refresh-docs/getting_started/install/#using-plain-kubernetes-manifests
+kubectl create argocd
+kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
