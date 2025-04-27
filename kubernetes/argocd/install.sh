a0b025f9bdd85f8c20318739f41b0f26e88ecda1
dortlii <22558338+dortlii@users.noreply.github.com>
2025-04-27 10:48:32 +0200

    Add custom health checks for cert-manager and Ingress in patch.yaml

--- kubernetes/argocd/install.sh
+++ kubernetes/argocd/install.sh
@@ -6,3 +6,5 @@
 # https://kostis-argo-cd.readthedocs.io/en/refresh-docs/getting_started/install/#using-plain-kubernetes-manifests
 kubectl create argocd
 kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl -n argocd patch configmap argocd-cm --patch-file patch.yaml
