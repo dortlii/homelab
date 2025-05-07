b072d05830cfef4935da484ddf24ac0ab70be02e
dortlii <22558338+dortlii@users.noreply.github.com>
2025-05-07 20:09:02 +0200

    Add sway script

---
+++ shell/sway.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# =============================================================================
+# Script Name:    sway.sh
+# Description:    Sets up a sway installation for personal usage
+# Usage:          ./sway.sh
+# Author:         Fabian Dort <fabian@dortlii.dev>
+# Maintainer:     Fabian Dort <fabian@dortlii.dev>
+# Version:        1.0.0
+# License:        GNU GPLv3
+# Dependencies:
+#
+# History:
+#   Version 1.0.0 - Initial release
+# =============================================================================
+
+#-----------------------------------------
+# functions
+#-----------------------------------------
+beauty_output() {
+  echo "#-----------------------------------------------------------------------------------------------"
+}
+
+#-----------------------------------------
+# install tools with dnf
+#-----------------------------------------
+YAY_TOOLS=(
+  "sway"
+  "swaybg"
+  "hyprland-polkit"
+)
+
+beauty_output
+echo "installing all the yay tools ..."
+yay -Sq "${YAY_TOOLS[@]}"
